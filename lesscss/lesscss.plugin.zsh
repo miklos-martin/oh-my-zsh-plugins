ffcaccb56b6baf97c7d3161d7b06735b6edcb593
mikka <miklos.martin@gmail.com>
2012-09-07 17:24:44 +0200

    added option (-x) to output minified css

--- lesscss/lesscss.plugin.zsh
+++ lesscss/lesscss.plugin.zsh
@@ -3,5 +3,30 @@
 # Requires watchr: https://github.com/mynyml/watchr
 
 watchless () {
-    watchr -e 'watch(".*\.less$") { |f| system("lessc #{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css && echo \"#{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css\" ") }'
+    compressed=0;
+    
+    while getopts ":xh" option
+    do
+      case $option in
+        x ) compressed=1 ;;
+        h ) _watchless_usage; return ;;
+      esac
+    done
+
+    if [ $compressed -eq 1 ]; then
+        x=' -x'
+    else
+        x=''
+    fi
+    
+    watchr -e 'watch(".*\.less$") { |f| system("lessc #{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css'$x' && echo \"#{f[0]} > $(echo #{f[0]} | cut -d\. -f1).css\" ") }'
+}
+
+_watchless_usage () {
+    echo "Usage: watchless [options]"
+    echo
+    echo "Options"
+    echo "  -x   Compiles less files into minified css files"
+    echo "  -h   Get this help message"
+    return
 }
